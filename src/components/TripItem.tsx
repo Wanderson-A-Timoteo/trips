ad5e569699a13b5a506709c0f32c2b6b27ea74ea
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-09-02 17:25:03 -0400

    feat: added initial trip details page and header

--- src/components/TripItem.tsx
+++ src/components/TripItem.tsx
@@ -1,5 +1,6 @@
 import { Trip } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import ReactCountryFlag from "react-country-flag";
 
@@ -9,27 +10,29 @@ interface TripItemProps {
 
 const TripItem = ( { trip } : TripItemProps) => {
   return(
-    <div className="flex flex-col">
-      <div className="relative h-[280px] w-[300px]">
-        <Image 
-          className="rounded-lg shadow-md"
-          src={trip.coverImage} 
-          alt={trip.name}
-          style={{
-            objectFit: "cover"
-          }}
-          fill
-        />
+    <Link href={`/trips/${trip.id}`}>
+      <div className="flex flex-col">
+        <div className="relative h-[280px] w-[300px]">
+          <Image 
+            className="rounded-lg shadow-md"
+            src={trip.coverImage} 
+            alt={trip.name}
+            style={{
+              objectFit: "cover"
+            }}
+            fill
+          />
+        </div>
+        <h3 className="text-primaryDarker font-medium text-sm mt-2"> { trip.name } </h3>
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg />
+          <p className="text-xs text-grayPrimary"> { trip.location } </p>
+        </div>
+        <p className="text-xs text-grayPrimary"> 
+          <span className="text-primary font-medium"> R$ { trip.pricePerDay.toString() } </span> diária
+        </p>
       </div>
-      <h3 className="text-primaryDarker font-medium text-sm mt-2"> { trip.name } </h3>
-      <div className="flex items-center gap-1 my-1">
-        <ReactCountryFlag countryCode={trip.countryCode} svg />
-        <p className="text-xs text-grayPrimary"> { trip.location } </p>
-      </div>
-      <p className="text-xs text-grayPrimary"> 
-        <span className="text-primary font-medium"> R$ { trip.pricePerDay.toString() } </span> diária
-      </p>
-    </div>
+    </Link>
   );
 };
 export default TripItem;
